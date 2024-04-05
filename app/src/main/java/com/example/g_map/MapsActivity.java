5079110ef9b2dea8bff9f0b1d9fabd2a5e07c8e1
ayushsingh-22 <ayushkkumar2205@gmail.com>
2024-04-06 00:23:09 +0530

    final trial

--- app/src/main/java/com/example/g_map/MapsActivity.java
+++ app/src/main/java/com/example/g_map/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.example.g_map;
 
+//import static com.example.g_map.Manifest.*;
+
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -29,23 +35,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng location = new LatLng(28.70, 77.102);
+        System.out.println("Value  ---> "+location);
+        mMap.addMarker(new MarkerOptions().position(location).title("Marker in New Delhi"));
+        System.out.println("Value 2 ---> "+location);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,25));
+        System.out.println("Value 3 ---> "+location);
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,10));
+//        System.out.println("Value 4 ---> "+location);
     }
 }
